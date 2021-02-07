28d88004b015dfff9cf42df7cc5b2254724d43bb
fangxq <xiaoqing.fang@quvideo.com>
2021-02-07 17:22:46 +0800

    feat：交互

---
+++ app/src/main/java/com/fxq/kotlin/mvvm/ui/fragment/ColorPickerFragment.kt
@@ -0,0 +1,84 @@
+package com.fxq.kotlin.mvvm.ui.fragment
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.util.DisplayMetrics
+import android.view.Gravity
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import com.fxq.lib.widget.colorpicker.ColorPicker
+import com.fxq.lib.widget.colorpicker.OnColorChangeListener
+import com.hazz.kotlinmvp.R
+
+/**
+ * Author: Fanxq
+ * Date: 2021/2/7
+ */
+class ColorPickerFragment : DialogFragment() {
+
+  var mOnColorChangeListener: OnColorChangeListener? = null
+  private lateinit var mColorPicker: ColorPicker
+
+  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    val activity: Activity = activity!!
+    val view = getDialogView(activity!!)
+    val builder = AlertDialog.Builder(activity)
+      .setView(view)
+    return builder.create()
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    updatePosition()
+    return super.onCreateView(inflater, container, savedInstanceState)
+  }
+
+  private fun getDialogView(activity: Activity): View? {
+    val view: View = LayoutInflater.from(activity).inflate(R.layout.fragment_color_picker, null)
+    mColorPicker = view.findViewById(R.id.cpv_color_picker)
+    mColorPicker.setOnColorChangeListener(mOnColorChangeListener)
+    return view
+  }
+
+  override fun onStart() {
+    super.onStart()
+    val window = dialog.window
+    val windowParams = window!!.attributes
+    windowParams.dimAmount = 0.0f
+    window.attributes = windowParams
+    val manager = window.windowManager
+    val outMetrics = DisplayMetrics()
+    manager.defaultDisplay.getMetrics(outMetrics)
+    window.decorView.setPadding(0, 0, 0, 0)
+    window.setBackgroundDrawableResource(android.R.color.transparent)
+    dialog.setCanceledOnTouchOutside(true)
+    dialog.setCancelable(true)
+    dialog.window
+      ?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    dialog.setOnKeyListener { dialog, keyCode, event ->
+      if (keyCode === KeyEvent.KEYCODE_BACK) {
+        dismissAllowingStateLoss()
+        return@setOnKeyListener true
+      }
+      false
+    }
+  }
+
+  fun updatePosition() {
+    val dialog = dialog ?: return
+    val window = dialog.window
+    val layoutParams = window!!.attributes
+    layoutParams.gravity = Gravity.BOTTOM
+    layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT
+    window.attributes = layoutParams
+  }
+}
--- app/src/main/java/com/hazz/kotlinmvp/ui/activity/TestActivity.kt
+++ app/src/main/java/com/hazz/kotlinmvp/ui/activity/TestActivity.kt
@@ -1,12 +1,14 @@
 package com.hazz.kotlinmvp.ui.activity
 
 import android.util.Log
+import android.view.View
 import android.widget.TextView
+import com.fxq.kotlin.mvvm.ui.fragment.ColorPickerFragment
 import com.fxq.lib.widget.colorpicker.OnColorChangeListener
 import com.hazz.kotlinmvp.R
 import com.hazz.kotlinmvp.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_test.cpv_color_picker
-import kotlinx.android.synthetic.main.activity_test.tv_test
+import kotlinx.android.synthetic.main.activity_test.tv_show
 
 /**
  * Author: Fanxq
@@ -14,6 +16,7 @@ import kotlinx.android.synthetic.main.activity_test.tv_test
  */
 class TestActivity : BaseActivity() {
 
+  private var colorPickerFragment: ColorPickerFragment?= null
   override fun layoutId(): Int {
     return R.layout.activity_test
   }
@@ -25,7 +28,17 @@ class TestActivity : BaseActivity() {
     cpv_color_picker.setOnColorChangeListener(object : OnColorChangeListener {
       override fun colorChanged(color: Int) {
         Log.e("fxq", "color = $color")
-        tv_test.setBackgroundColor(color)
+        //tv_show.setBackgroundColor(color)
+      }
+    })
+    tv_show.setOnClickListener(View.OnClickListener {
+      //cpv_color_picker.visibility = View.VISIBLE
+      colorPickerFragment?.let {
+        it.show(supportFragmentManager, "colorPicker")
+      } ?: ColorPickerFragment().let {
+        colorPickerFragment = it
+        it.mOnColorChangeListener = OnColorChangeListener { color -> tv_show.setBackgroundColor(color) }
+        it.show(supportFragmentManager, "colorPicker")
       }
     })
   }
